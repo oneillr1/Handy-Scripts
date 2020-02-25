90828dd8655db78ef0dce2851133d2c5a01b68d1
oneillr1 <oneillr1@tcnj.edu>
2020-02-25 16:29:14 -0500

    updated script

--- get_instance_launchtime.py
+++ get_instance_launchtime.py
@@ -1,6 +1,17 @@
-import requests
 import json
+import datetime
+from datetime import date
+from datetime import datetime
+import boto3
 
+
+def get_instances():
+
+    boto3.setup_default_session(profile_name='int')
+    client = boto3.client('ec2')
+    response = client.describe_instances()
+    print(response)
+    return 
 
 def get_dates(dates):
     with open('instance_info.json', 'r') as f:
@@ -22,15 +33,30 @@ def get_dates(dates):
 
 
 def how_old(dates):
+    today = date.today()
+    for x in dates:
+        temp = datetime.strptime(x[1],'%Y-%m-%dT%H:%M:%S.%fZ').date()
+        x[1]=int((today-temp).days)
     return dates
 
 def sort_by_oldest(dates):
-    return dates
+    
+    return(sorted(dates, key = lambda x: x[1],reverse = True)) 
+    
 
 
 def main():
     dates = []
+    get_instances()
     dates = get_dates(dates)
+    print('\n')
+    print(dates)
+    dates = how_old(dates)
+    print('\n')
+    print(dates)
+    dates = sort_by_oldest(dates)
+    print('\n')
+    print(dates)
 
 if __name__ == "__main__":
     main()
